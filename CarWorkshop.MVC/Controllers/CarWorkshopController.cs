244e2da9aab416b748131fa4406f87ba1ba884b2
Maciej Obarzanek <maciej.obarzanek@gmail.com>
2023-04-20 15:19:29 +0200

    Little code format + return status ok after car workshop create.

--- CarWorkshop.MVC/Controllers/CarWorkshopController.cs
+++ CarWorkshop.MVC/Controllers/CarWorkshopController.cs
@@ -1,21 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using CarWorkshop.Application.Services;
 
-namespace CarWorkshop.MVC.Controllers
-{
-    public class CarWorkshopController : Controller
-    {
-        private readonly ICarWokrshopService _carWorkshopService;
-        public CarWorkshopController(ICarWokrshopService carWokrshopService)
-        {
-            _carWorkshopService = carWokrshopService;
-        }
+namespace CarWorkshop.MVC.Controllers;
 
-        [HttpPost]
-        public async Task<IActionResult> Create(Domain.Entities.CarWorkshop carWorkshop)
-        {
-            await _carWorkshopService.Create(carWorkshop);
-            return null;
-        }
+public class CarWorkshopController : Controller
+{
+    private readonly ICarWokrshopService _carWorkshopService;
+    public CarWorkshopController(ICarWokrshopService carWokrshopService)
+    {
+        _carWorkshopService = carWokrshopService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Domain.Entities.CarWorkshop carWorkshop)
+    {
+        await _carWorkshopService.Create(carWorkshop);
+        return Ok(carWorkshop);
     }
 }
